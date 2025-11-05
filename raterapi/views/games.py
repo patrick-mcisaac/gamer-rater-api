48a23a542de6b8e39c05ef52ddefecf496c220a9
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-11-05 10:00:03 -0600

    retrieve single game from db working

--- raterapi/views/games.py
+++ raterapi/views/games.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status, serializers
-from raterapi.models import Game
+from raterapi.models import Game, Category
 
 
 class GameViewSet(ViewSet):
@@ -11,8 +11,25 @@ class GameViewSet(ViewSet):
         serializer = GameSerializer(games, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
 
+    def retrieve(self, request, pk=None):
+        try:
+            game = Game.objects.get(pk=pk)
+            serializer = GameSerializer(game, many=False)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Game.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class GameCategoriesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
+
 
 class GameSerializer(serializers.ModelSerializer):
+
+    categories = GameCategoriesSerializer(many=True)
 
     class Meta:
         model = Game
